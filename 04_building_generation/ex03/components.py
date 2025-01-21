4c32b98d24388bee41fca31d73aed74649293cf6
bbeemt <b.t.vandenbeemt@student.tudelft.nl>
2025-01-21 10:20:09 +0100

    toegevoegde components

--- 04_building_generation/ex03/components.py
+++ 04_building_generation/ex03/components.py
@@ -5,6 +5,7 @@ from numpy.random import randint, rand
 """
 Materials are used to define the appearance of a mesh.
 """
+'''
 material_stone_bricks = bk.Material()
 material_stone_bricks.textures = {
     "diffuse_texture": bk.res_path("../03_textures/assets/stone_bricks_col.jpg"),
@@ -18,10 +19,10 @@ material_basic_bricks.specular = bk.Color(0.1, 0.1, 0.1)
 material_basic_bricks.textures = {
     "diffuse_texture": bk.res_path("../assets/brick.jpg"),
 }
-
+'''
 material_basic_floor = bk.Material()
 material_basic_floor.diffuse = bk.Color(0.8, 0.5, 0.5)
-
+'''
 material_basic_window = bk.Material()
 material_basic_window.textures = {
     "diffuse_texture": bk.res_path("../assets/window.jpg"),
@@ -36,12 +37,59 @@ material_basic_door = bk.Material()
 material_basic_door.textures = {
     "diffuse_texture": bk.res_path("../assets/door.jpg"),
 }
-
+'''
 material_highrise_window = bk.Material()
 material_highrise_window.textures = {
-    "diffuse_texture": bk.res_path("../assets/highrise_window.jpg")
+    "diffuse_texture": bk.res_path("../assets/Highrise_window.jpg")
 }
-class BasicWall(bk.Mesh):
+
+material_highrise_door = bk.Material()
+material_highrise_door.textures = {
+    "diffuse_texture": bk.res_path("../assets/Highrise_door.jpg")
+}
+
+material_highrise_lower_window = bk.Material()
+material_highrise_lower_window.textures = {
+    "diffuse_texture": bk.res_path("../assets/Highrise_lower_window.jpg")
+}
+
+material_skyscraper_window1 = bk.Material()
+material_skyscraper_window1.textures = {
+    "diffuse_texture": bk.res_path("../assets/Skyscraper_window1.jpg")
+}
+
+material_skyscraper_window2 = bk.Material()
+material_skyscraper_window2.textures = {
+    "diffuse_texture": bk.res_path("../assets/Skyscraper_window2.jpg")
+}
+
+material_skyscraper_window3 = bk.Material()
+material_skyscraper_window3.textures = {
+    "diffuse_texture": bk.res_path("../assets/Skyscraper_window3.jpg")
+}
+
+material_skyscraper_window4 = bk.Material()
+material_skyscraper_window4.textures = {
+    "diffuse_texture": bk.res_path("../assets/Skyscraper_window4.jpg")
+}
+
+material_skyscraper_door = bk.Material()
+material_skyscraper_door.textures = {
+    "diffuse_texture": bk.res_path("../assets/Skyscraper_door.jpg")
+}
+
+material_office_window = bk.Material()
+material_office_window.textures = {
+    "diffuse_texture": bk.res_path("../assets/Office_window.jpg")
+}
+
+material_office_door = bk.Material()
+material_office_door.textures = {
+    "diffuse_texture": bk.res_path("../assets/Office_door.jpg")
+}
+
+
+class HighriseWindow(bk.Mesh):
     """
     Create a basic wall mesh with the given size and material.
     This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
@@ -52,11 +100,11 @@ class BasicWall(bk.Mesh):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
 
-    def __init__(self, w=1, h=1, m=material_basic_bricks):
+    def __init__(self, w=1, h=1, m=material_highrise_window):
         super().__init__()
         self.w = w
         self.h = h
-        self.name = "BasicWallMesh"
+        self.name = "HighriseWindow"
         self.positions = [
             [-w / 2, -h / 2, 0],
             [w / 2, -h / 2, 0],
@@ -67,6 +115,241 @@ class BasicWall(bk.Mesh):
         self.triangles = [[0, 1, 2], [0, 2, 3]]
         self.materials = [m]
 
+class HighriseDoor(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_highrise_door):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "HighriseDoor"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class HighriseLowerWindow(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_highrise_lower_window):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "HighriseLowerWindow"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+
+class SkyscraperWindow1(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_skyscraper_window1):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "SkyscraperWindow1"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+
+class SkyscraperWindow2(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_skyscraper_window2):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "SkyscraperWindow2"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class SkyscraperWindow3(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_skyscraper_window3):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "SkyscraperWindow3"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class SkyscraperWindow4(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_skyscraper_window4):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "SkyscraperWindow4"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class SkyscraperDoor(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_skyscraper_door):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "SkyscraperDoor"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class OfficeWindow(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_office_window):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "OfficeWindow"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
+
+class OfficeDoor(bk.Mesh):
+    """
+    Create a basic wall mesh with the given size and material.
+    This class is a subclass of bk.Mesh, so it can be used as a mesh. For example,
+    you can create a mesh instance by `mesh = BasicWallMesh(...)`, and then add it to
+    a scene by `app.add_mesh(mesh)`. It's the same as using `mesh = create_basic_wall(...)`.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, w=1, h=1, m=material_office_door):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.name = "OfficeDoor"
+        self.positions = [
+            [-w / 2, -h / 2, 0],
+            [w / 2, -h / 2, 0],
+            [w / 2, h / 2, 0],
+            [-w / 2, h / 2, 0],
+        ]
+        self.texcoords = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.triangles = [[0, 1, 2], [0, 2, 3]]
+        self.materials = [m]
 
 class BasicFloor(bk.Mesh):
     def __new__(cls, *args, **kwargs):
@@ -89,6 +372,7 @@ class BasicFloor(bk.Mesh):
         self.materials = [m]
 
 
+'''
 class BasicWindowWall(bk.Mesh):
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
@@ -154,3 +438,4 @@ class BasicDoorWall(bk.Mesh):
             bk.SubMesh(0, 6, 0),
             bk.SubMesh(6, 8, 1),
         ]
+'''
